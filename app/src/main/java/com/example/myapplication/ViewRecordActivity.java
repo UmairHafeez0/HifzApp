1390695568fe3ada237024b9f50f7534794c7489
UmairHafeez0 <ra3927538@gmail.com>
2023-06-18 08:54:42 +0500

    Version15

--- app/src/main/java/com/example/myapplication/ViewRecordActivity.java
+++ app/src/main/java/com/example/myapplication/ViewRecordActivity.java
@@ -1,4 +1,45 @@
 package com.example.myapplication;
 
-public class ViewRecordActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ViewRecordActivity extends AppCompatActivity {
+
+
+    List<StudentRecord> friendsList = new ArrayList<>();
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewrecord);
+
+
+
+        friendsList.addAll(Arrays.asList(new Friends[]{f0,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11}));
+        recyclerView = findViewById(R.id.recylerViewStudent);
+
+
+        recyclerView.setHasFixedSize(true);
+
+        //LinearLayoutManager GridLayoutManager
+        layoutManager = new LinearLayoutManager(ViewRecordActivity.this);
+//        layoutManager = new LinearLayoutManager(MainActivity.this,
+//                LinearLayoutManager.HORIZONTAL,
+//                false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new myRecyclerViewAdapter(friendsList) ;
+        recyclerView.setAdapter(adapter);
+        //adapter.notifyDataSetChanged();
 }
---
+++ app/src/main/java/com/example/myapplication/myRecyclerViewAdapter.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class myRecyclerViewAdapter extends RecyclerView.Adapter {
+    public myRecyclerViewAdapter(List<StudentRecord> friendsList) {
+    }
+}
