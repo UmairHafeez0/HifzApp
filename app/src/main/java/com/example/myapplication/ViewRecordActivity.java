a490dbe99ac6d531f99515aca31d77609c75b489
UmairHafeez0 <ra3927538@gmail.com>
2023-06-18 09:25:22 +0500

    Version20

--- app/src/main/java/com/example/myapplication/ViewRecordActivity.java
+++ app/src/main/java/com/example/myapplication/ViewRecordActivity.java
@@ -7,17 +7,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ViewRecordActivity extends AppCompatActivity {
-
 
     List<StudentRecord> friendsList = new ArrayList<>();
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
-
+    DbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +24,17 @@ public class ViewRecordActivity extends AppCompatActivity {
 
         String rollNo = getIntent().getStringExtra("rollNo");
 
-        friendsList.addAll(Arrays.asList(new Friends[]{f0,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11}));
+        dbHelper = new DbHelper(this);
+
+        friendsList = dbHelper.searchStudent(rollNo);
+
         recyclerView = findViewById(R.id.recylerViewStudent);
-
-
         recyclerView.setHasFixedSize(true);
 
-        //LinearLayoutManager GridLayoutManager
         layoutManager = new LinearLayoutManager(ViewRecordActivity.this);
-//        layoutManager = new LinearLayoutManager(MainActivity.this,
-//                LinearLayoutManager.HORIZONTAL,
-//                false);
         recyclerView.setLayoutManager(layoutManager);
 
-        adapter = new myRecyclerViewAdapter(friendsList) ;
+        adapter = new myRecyclerViewAdapter(friendsList);
         recyclerView.setAdapter(adapter);
-        //adapter.notifyDataSetChanged();
+    }
 }
--- app/src/main/java/com/example/myapplication/myRecyclerViewAdapter.java
+++ app/src/main/java/com/example/myapplication/myRecyclerViewAdapter.java
@@ -4,7 +4,6 @@ package com.example.myapplication;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
